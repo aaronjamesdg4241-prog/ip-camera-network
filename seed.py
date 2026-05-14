99fbc6c408e0772d7706f7b3fb4c9f23521b57f2
aaronjamesdguzman-oss <aaronjamesdguzman@gmail.com>
2026-05-14 11:09:12 +0800

    seed.py

--- seed.py
+++ seed.py
@@ -1,7 +1,15 @@
 from app import app, db, User
+from werkzeug.security import generate_password_hash
 
 with app.app_context():
-    user = User(username='admin', password='admin')
-    db.session.add(user)
-    db.session.commit()
-    print("Admin user created")
+    # Check if user already exists
+    existing_user = User.query.filter_by(username='admin').first()
+    if not existing_user:
+        hashed_password = generate_password_hash('admin', method='sha256')
+        user = User(username='admin', password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        print("Admin user created")
+    else:
+        print("Admin user already exists")
+        
